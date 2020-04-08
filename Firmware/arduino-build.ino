4bb57e71f1c980c32873e4129c9b47f927e26516
NickTheBit <nikosgklu@gmail.com>
2020-04-08 17:44:06 +0300

    Added support for all buttons
    Right Ok and Left

--- Firmware/arduino-build.ino
+++ Firmware/arduino-build.ino
@@ -1,7 +1,10 @@
 #include "BluetoothSerial.h"
 #include "Arduino_JSON.h"
+#include <RTClib.h>
 
-const int PushButton = 16;
+const int Left = 15;
+const int Right = 17;
+const int Ok = 13;
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -12,7 +15,10 @@ BluetoothSerial SerialBT;
 JSONVar packet;
 
 void setup() {
-  pinMode(PushButton, INPUT);
+  pinMode(Left, INPUT);
+  pinMode(Ok, INPUT);
+  pinMode(Right, INPUT);
+  
   Serial.begin(115200);
   SerialBT.begin("PSW"); //Bluetooth device name
   Serial.println("The device online, standing by...");
@@ -21,11 +27,14 @@ void setup() {
 void loop() {
   // Preparing json packet
   packet["left"] = 0;
+  packet["ok"] = 0;
   packet["right"] = 0;
 
   int update_flag = 0;
   
-  int Push_button_state = digitalRead(PushButton);
+  int L_Button = digitalRead(Left);
+  int R_Button = digitalRead(Right);
+  int O_Button = digitalRead(Ok);
   
   if (Serial.available()) {
     SerialBT.write(Serial.read());
@@ -34,14 +43,23 @@ void loop() {
     Serial.write(SerialBT.read());
   }
   
-  if (Push_button_state == HIGH) {
+  if (L_Button == HIGH) {
     update_flag = 1;
-    Serial.write("Button pressed\n");
-    SerialBT.println("Button pressed");
     packet["left"] = 1;
-    delay(300);
+    delay(50);
   }
-  delay(20);
+  if (R_Button == HIGH) {
+    update_flag = 1;
+    packet["right"] = 1;
+    delay(50);
+  }
+  if (O_Button == HIGH) {
+    update_flag = 1;
+    packet["ok"] = 1;
+    delay(50);
+  }
+  
+  delay(300);
   if (update_flag==1) {
     SerialBT.println(packet);
     Serial.println(packet);
